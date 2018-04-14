db99d264602d8814bc30000bba742ebd8490e981
Patrick Huber <patrick@huber.com>
2018-04-14 10:57:22 -0400

    updates merge

--- deploy-jumpbox.sh
+++ deploy-jumpbox.sh
@@ -1,7 +1,7 @@
 bosh create-env submodules/github.com/cppforlife/jumpbox-deployment/jumpbox.yml \
      --state ./state.json \
-     -o <(bosh int submodules/github.com/cppforlife/jumpbox-deployment/vsphere/cpi.yml -o ops-files/update-stemcell-release.yml) \
      -o submodules/github.com/cppforlife/jumpbox-deployment/no-external-ip.yml \
+     -o ops-files/run-install.yml \
      --vars-store no-commit/creds.yml \
      -l vars-files/lab.yml \
      -l no-commit/lab.yml
